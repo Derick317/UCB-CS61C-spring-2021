5927a6c3cac23858c65d9e96723dc94f9a516868
Derick317 <derick.chen@outlook.com>
2022-12-30 11:43:16 +0800

    Add my solutions to Lab 3

---
+++ labs/lab03/factorial.s
@@ -0,0 +1,36 @@
+.globl factorial
+
+.data
+n: .word 8
+
+.text
+main:
+    la t0, n
+    lw a0, 0(t0)
+    jal ra, factorial
+
+    addi a1, a0, 0
+    addi a0, x0, 1
+    ecall # Print Result
+
+    addi a1, x0, '\n'
+    addi a0, x0, 11
+    ecall # Print newline
+
+    addi a0, x0, 10
+    ecall # Exit
+
+factorial:
+    # YOUR CODE HERE
+    addi t0, x0, 1 # t0 stores the result
+    add t1, x0, a0 # t1 is the multiplier
+loop:
+    beq t1, x0, exit
+    # Now, t1 != 0, an iteration starts
+    mul t0, t0, t1
+    addi t1, t1, -1
+    jal x0, loop
+exit:
+    add a0, x0, t0
+    jr ra
+    
